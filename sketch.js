a0f0aecaee849e1ff0e68790bc5db1f7e10bcfd8
abhishek2326 <91264173+abhishek2326@users.noreply.github.com>
2021-10-14 22:07:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+function preload(){
+ // pre-load images
+}path.addImage(pathImg);
+path.velocityY = 4;
+path.scale=1.2;
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+path=createSprite()
+}
+
+function draw() {
+  background(black);
+
+}
+
